031847ad91166e7ac6a7d7c036fc061cf4be41b2
Valentin Cmn <valentincmn@MacBook-Pro-de-Valentin.local>
2025-11-02 22:00:32 +0100

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (
+        <nav>
+            <div className='container mx-auto px-4 py-3'>
+                <h1 className='text-base text-[#E43F12]'>Luméa</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar; 
